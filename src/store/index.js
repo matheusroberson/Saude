9585b7b405efd62a3085b490eb61d16579fcf79c
Matheus Roberson <mewrso@gmail.com>
2021-10-04 11:59:58 -0300

    Create actions and getters

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    name: "",
+    cpf: "",
+    cel: "",
+    state: "",
+    city: "",
+    specialty: "",
+    price: 0,
+    payment: "",
+  },
+
+  mutations: {
+    setCurrentData(state, payload) {
+      Object.keys(payload).forEach((key) => {
+        Vue.set(state, key, payload[key]);
+      });
+    },
+  },
+
+  actions: {
+    setCurrentData: ({ commit }, data) => commit("setCurrentData", data),
+  },
+
+  getters: {
+    getCurrentData: (state) => state,
+  },
+});
